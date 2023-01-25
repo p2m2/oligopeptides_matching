75bd0937634ccc223bf3036ed23fb1554d69736d
Kevin Billet <kevin.billet@inrae.fr>
2023-01-25 10:36:08 +0100

    change the return type from a matrix to a data.frame close #1

--- R/build_mz_peptides.R
+++ R/build_mz_peptides.R
@@ -14,14 +14,14 @@
 #' @param peptide list of oligopeptide.
 #' @param oligomerization_degree integer : oligomerization degree  
 #' @param ionization : list of ionization
-#' @return matrix containing OD, Id , Mz , ([M - Xionization ], [M + Xionization])+ .
+#' @return a data.frame containing OD, Id , Mz , ([M - Xionization ], [M + Xionization])+ .
 #' @export
 #' @examples 
 #' build_mz_peptides(stats::setNames(c(160.0848,192.0569),c("A_A","A_C")), 2, stats::setNames(c(1.007825),c("H")))
 build_mz_peptides <- function(peptide,
                               oligomerization_degree,
                               ionization){
-  peptide_matrix <- matrix(data=NA, nrow=length(peptide), ncol=3+2*length(ionization))
+  peptide_matrix <- as.data.frame(matrix(data=NA, nrow=length(peptide), ncol=3+2*length(ionization)))
   header <- c("OD", "id", "MW")
   for (i in seq(from=1, to=length(ionization), by=2)){
     header = append(header, paste("[M-", names(ionization)[i],"]", sep=""))
