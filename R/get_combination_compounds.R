d18eb49f8c0a14eb6e9b6d459f573cb018dab1fe
Kevin Billet <kevin.billet@inrae.fr>
2023-02-01 10:10:21 +0100

    conclusion about the combination of amino acids/oligopeptides and polyphenols

--- R/get_combination_compounds.R
+++ R/get_combination_compounds.R
@@ -12,16 +12,17 @@ combined_compounds_mass <- vector("numeric", m*n)
 
 k = 1
 
-for (i in 1:m*n){
-    # nameOligo <- names(mass_oligo_vector_np1)[i]
-    # l <- stringr::str_sort(append(unlist(stringr::str_split(nameOligo, "_")),names(mass_aa_vector)[j]))
-    
-    combined_compounds_names[k]=paste(names(compounds_1), names(compounds_2), collapse="_")
-    combined_compounds_mass[k]=compounds_1[k]+compounds_2[k]-chemical_reaction
-     k <- k+1
+for (i in 1:n){
+  for (j in 1:m){
+    combined_compounds_names[k]=paste(names(compounds_1)[i], names(compounds_2)[j], sep="_")
+    combined_compounds_mass[k]=compounds_1[i]+compounds_2[j]-chemical_reaction
+    k <- k+1
   }
+}
 
 combined_compounds <- (stats::setNames(combined_compounds_mass,combined_compounds_names))
 combined_compounds <- combined_compounds[unique(names(combined_compounds))]
+
 return(combined_compounds)
 }
+
