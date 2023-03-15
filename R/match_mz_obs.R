04f1da75e4d77ff2f5c6a3c55f9b7b9d1fbec052
Olivier Filangi <you@example.com>
2023-03-15 11:59:40 +0100

    add import test

--- R/match_mz_obs.R
+++ R/match_mz_obs.R
@@ -6,15 +6,16 @@
 #' @param combined_compounds is the combined compounds array to match
 #' @param ppm_error is the criteria to match compounds
 #' @return the list as dataframe of all the matches
+#' @importFrom rlang .data
 #' @export
 
 match_mz_obs <- function(mz_obs,
                          combined_compounds,
-                         ppm_error=5) {
+                         ppm_error = 5) {
   return(combined_compounds %>%
            filter(combined_compounds < round(mz_obs, digits = 0) + 1) %>%
            filter(combined_compounds >= round(mz_obs, digits = 0)) %>%
-           mutate(round = round(combined_compounds, digits = 4)) %>%
-           mutate(ppm_error_value = d_ppm(round, mz_obs)) %>%
+           dplyr::mutate(round = round(combined_compounds, digits = 4)) %>%
+           dplyr::mutate(ppm_error_value = d_ppm(round, mz_obs)) %>%
            filter(ppm_error_value<ppm_error))
 }
