624df8f0c6884fc742e1fe8201d6f899ca6f286f
Olivier Filangi <olivier.filangi@inrae.fr>
2023-11-16 10:03:36 +0100

    fix typo about mz. mz -> mw

--- R/get_oligopeptides.R
+++ R/get_oligopeptides.R
@@ -4,12 +4,12 @@
 #' Builds a matrix containing : 
 #'  - Oligomerization Degree
 #'  - Id (Combinaison of Amino Acid names)
-#'  - Mz (ionized mass)
+#'  - MW (molecular weight)
 #'  - ...
 #'
 #' @param aminoacids list of amino acids.
 #' @param oligomerization_degree integer : oligomerization degree
-#' @return matrix containing OD, Id , Mz ,M - Xionization,M + Xionization .
+#' @return matrix containing OD, Id , MW .
 #' @export
 #' @examples
 #' get_oligopeptides(aminoacids = stats::setNames(c(89.047679),c("A")),
@@ -17,12 +17,12 @@
 get_oligopeptides <- function(aminoacids,oligomerization_degree){
   H2O <- 18.010565
   tmp_oligopeptides <- aminoacids
-  all_mz_peptides <-  build_mz_peptides(aminoacids, 1)
+  all_peptides <-  build_peptides(aminoacids, 1)
   for (i in 1:(oligomerization_degree-1)){
     tmp_oligopeptides <- oligopeptides_building(tmp_oligopeptides, aminoacids,
     H2O)
-    tmp_mz_peptides <- build_mz_peptides(tmp_oligopeptides, i+1)
-    all_mz_peptides <- rbind(all_mz_peptides, tmp_mz_peptides)
+    tmp_peptides <- build_peptides(tmp_oligopeptides, i+1)
+    all_peptides <- rbind(all_peptides, tmp_peptides)
   }
-  return(all_mz_peptides)
+  return(all_peptides)
 }
