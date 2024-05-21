4190886b2b2695c930fead0f17653e52a8e66ad0
Sirine Oueida <sirine.oueida@inrae.fr>
2024-05-21 15:38:12 +0200

    data updated

--- oligopeptides_matching/data.R
+++ oligopeptides_matching/data.R
@@ -1,15 +1,25 @@
 # this file is used in the test. check R directory.
 # Constants
 ### Amino acids
+full_name <- c("Alanine", "Cysteine", "Aspartic acid", "Glutamic acid", "Phenylalanine", "Glycine", "Histidine", "Isoleucine", "Lysine", "Leucine", "Methionine", "Asparagine", "Proline", "Glutamine", "Arginine", "Serine", "Threonine", "Valine", "Tryptophan", "Tyrosine") 
+
+#Symbole à 3 lettres 
+symbol <- c("Ala", "Cys", "Asp", "Glu", "Phe", "Gly", "His", "Ile", "Lys", "Leu", "Met", "Asn", "Pro", "Gln", "Arg", "Ser", "Thr", "Val", "Trp", "Tyr")
+
 aa <- c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y")
-
-### amino acids molecular weight
 mass_aa <- c(89.047679, 121.019751, 133.037509, 147.053159, 165.078979, 
-              75.032029, 155.069477, 131.094629, 146.105528, 131.094629,
-              149.051051,132.053493, 115.063329, 146.069143, 174.111676,
-              105.042594,119.058244, 117.078979,204.089878, 181.073894)
+             75.032029, 155.069477, 131.094629, 146.105528, 131.094629,
+             149.051051,132.053493, 115.063329, 146.069143, 174.111676,
+             105.042594,119.058244, 117.078979,204.089878, 181.073894)
 
-aa_mw <- setNames(mass_aa, aa)
+# Polarity of each amino acid
+polarity <- c("Non-polaire", "Non-polaire", "Charge Negative", "Charge Negative", 
+              "Non-polaire", "Non-polaire", "Charge Positive", "Non-polaire", 
+              "Charge Positive", "Non-polaire", "Non-polaire", "Polaire", 
+              "Non-polaire", "Polaire", "Charge Positive", "Polaire", 
+              "Polaire", "Non-polaire", "Non-polaire", "Polaire")
+
+aa_mw <- data.frame(Full_Name = full_name, Symbol = symbol, Amino_Acid = aa, Mass = mass_aa, Specification_AA = polarity)
 
 ### polyphenol list
 name_polyphenol <- c("Cyanidin",
