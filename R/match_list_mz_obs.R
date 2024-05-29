f6cc1b95081a6c97fae8ecc54b1d9b1a97f88556
Sirine Oueida <sirine.oueida@inrae.fr>
2024-05-29 13:08:47 +0200

    script

---
+++ R/match_list_mz_obs.R
@@ -0,0 +1,16 @@
+#
+
+
+match_list_mz_obs <- function(name_combination,
+                              list_mz_obs,
+                              rt
+                         ) {
+  data_list <- data.frame()
+  for (mz_obs in list_mz_obs) { 
+    match_mass <- match_mz_obs(mz_obs, ionization, combined_compounds, ppm_error)
+    data_list <- rbind(data_list, match_mass)
+    
+  }
+  data_list <- dplyr::select(name_combination, list_mz_obs, rt, mz, mass, ppm_error_value)
+      return(data_list)
+  }
