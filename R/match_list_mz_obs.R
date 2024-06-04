b6cc14568cbe29f1d8d5a982212353399f1260cf
Sirine Oueida <sirine.oueida@inrae.fr>
2024-06-04 13:50:29 +0200

    version initial ancien

--- R/match_list_mz_obs.R
+++ R/match_list_mz_obs.R
@@ -11,20 +11,20 @@ match_list_mz_obs <- function(list_mz_obs,  # contient les mz, name, rt
   results <- list()
   
   for (i in 1:nrow(list_mz_obs)) {
-    feature_name <- list_mz_obs[i, "features_name"]
+    name <- list_mz_obs[i, "features_name"]
     mz <- list_mz_obs[i, "mz"]
     rt <- list_mz_obs[i, "rt"]
     
     match_mass <- match_mz_obs(mz_obs, ionization, combined_compounds, ppm_error)
     if (nrow(match_mass) > 0) {
-      match_mass <- cbind(feature_name, rt, match_mass)
+      match_mass <- cbind(name, rt, match_mass)
     } else {
       # Si aucun match n'est trouvé, ajouter une ligne avec NA pour les colonnes de match
-      match_mass <- data.frame(feature_name = feature_name, rt = rt,mz = NA, mz_obs = mz_obs, mass = NA, ppm_error_value = NA)
+      match_mass <- data.frame(name = name, rt = rt,mz = mz, mz_obs = mz_obs, mass = NA, ppm_error_value = NA)
     }
     
     results <- rbind(results, match_mass)
   }
-  results <- dplyr::select(results, feature_name, rt, mz, mz_obs, mass, ppm_error_value)
+  results <- dplyr::select(results, name, rt, mz, mz_obs, mass, ppm_error_value)
   return(results)
 }  
