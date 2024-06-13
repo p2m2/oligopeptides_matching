e691340267ea5f78fe245ce73116f6f83ea7274f
Sirine Oueida <sirine.oueida@inrae.fr>
2024-06-13 11:02:42 +0200

    new code done

--- R/match_list_mz.R
+++ R/match_list_mz.R
@@ -4,30 +4,31 @@
 # rbind : pour ajouter les résultats 
 # Sélection des colonnes
 
-match_list_mz_obs <- function(list_mz_obs,  # contient les mz, name, rt
-                              ionization,
-                              combined_compounds,
-                              ppm_error=5) {
-  results <- list()
+match_list_mz_obs <- function(list_mz_obs, ionization, combined_compounds, ppm_error=5) {
+  results <- list()  # Initialize an empty list to store results
   
   for (i in 1:nrow(list_mz_obs)) {
-    name <- list_mz_obs[i, "features_name"]
+    features_name <- list_mz_obs[i, "features_name"]
     mz <- list_mz_obs[i, "mz"]
     rt <- list_mz_obs[i, "rt"]
     
-    match_mass <- match_mz_obs(mz_obs, ionization, combined_compounds, ppm_error)
+    match_mass <- match_mz_obs(mz, ionization, combined_compounds, ppm_error)
+    
     if (nrow(match_mass) > 0) {
-      match_mass <- cbind(name, rt, match_mass)
+      match_mass <- cbind(features_name = features_name, rt = rt, match_mass)
     } else {
-      # Si aucun match n'est trouvé, ajouter une ligne avec NA pour les colonnes de match
-      match_mass <- data.frame(name = name, rt = rt,mz = mz, mz_obs = mz_obs, mass = NA, ppm_error_value = NA)
+      match_mass <- data.frame(features_name = features_name, rt = rt, mz_obs = mz, mass = NA, ppm_error_value = NA)
     }
     
-    results <- rbind(results, match_mass)
+    results[[i]] <- match_mass
   }
-  results <- dplyr::select(results, name, rt, mz, mz_obs, mass, ppm_error_value)
+  
+  results <- dplyr::bind_rows(results)
+  results <- dplyr::select(results, features_name, rt, mz_obs, mass, ppm_error_value)
   return(results)
-}  
+}
+
+
 example_list_mz_obs <- data.frame(
       name = c("M954t1417", "M100T50", "M121T93"),
       mz = c(953.6798, 100.07, 120.9659),
